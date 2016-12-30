8cb5347f2a9f43aeef6969b28910dc9e342a59eb
jagem88 <jagem88@gmail.com>
2016-12-30 11:50:28 -0500

    Add files via upload

---
+++ Front-End/js/btn3.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#btn3").click(function(){
+        $("#btn3").animate({
+            left: '500px',
+            height: '150px',
+            width: '150px'
+        });
+    });
+});
